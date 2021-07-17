0ebef63ba1546fa7db2079824a630d21001ec367
Christophe Salou <bezedache29@gmail.com>
2021-07-17 23:51:19 +0200

    Liste des animaux depuis la BDD

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,12 @@ class AnimalController extends AbstractController
     /**
      * @Route("/animaux", name="animaux")
      */
-    public function animaux(): Response
+    public function animaux(AnimalRepository $animalRepository): Response
     {
-        return $this->render('animal/animaux.html.twig');
+        $animaux = $animalRepository->findAll();
+
+        return $this->render('animal/animaux.html.twig', [
+            'animaux' => $animaux
+        ]);
     }
 }
