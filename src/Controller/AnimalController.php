1fa62916cc13c7729bac84bdc33c4709693f900d
Christophe Salou <bezedache29@gmail.com>
2021-07-18 01:49:33 +0200

    Détails d'un animal

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
 use App\Repository\AnimalRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AnimalController extends AbstractController
 {
@@ -18,6 +19,19 @@ class AnimalController extends AbstractController
 
         return $this->render('animal/animaux.html.twig', [
             'animaux' => $animaux
+        ]);
+    }
+
+    /**
+     * @Route("/animal/{id}", name="animal")
+     */
+    public function animal(Animal $animal): Response
+    {
+        // L'id passé dans l'url permet de récupérer l'animal passé dans les paramètres de la fonction
+        // Symfony le fait tout seul
+        
+        return $this->render('animal/animal.html.twig', [
+            'animal' => $animal
         ]);
     }
 }
